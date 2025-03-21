2caf9b8b1f7bee583c1e05974de66c35d17f9630
simontamarag <simontamarag@gmail.com>
2025-03-21 14:14:01 -0500

    feat:capacitor .com.ejemplo.AkibaNet

---
+++ android/app/src/main/java/com/ejemplo/AkibaNet/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ejemplo.AkibaNet;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ejemplo.AkibaNet',
+  appName: 'AkibaNet',
+  webDir: 'dist'
+};
+
+export default config;
