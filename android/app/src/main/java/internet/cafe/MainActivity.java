b6766a30bed01cc631f72a60865562acc2ddd4de
Nestor Gutierrez <nestorg456k@outlook.com>
2025-03-21 11:29:59 -0500

    feat: initialize Android project structure with Capacitor integration and basic configurations

---
+++ android/app/src/main/java/internet/cafe/MainActivity.java
@@ -0,0 +1,5 @@
+package internet.cafe;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'internet.cafe',
+  appName: 'internet-cafe',
+  webDir: 'dist'
+};
+
+export default config;
